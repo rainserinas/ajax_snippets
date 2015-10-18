5822d206117f96ab153ad57cc468d04af5a5b667
rainserinas <raineirserinas@gmail.com>
2015-10-18 23:52:22 +0800

    OOP based c# project

---
+++ OOProject/OOProject/CLASS/Class_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOProject.CLASS
+{
+    class Class_1
+    {
+
+        public String test() {
+
+            return "Hello Mr. Raineir John Serinas";
+        }
+
+        public int number(int num1,int num2) {
+            return num1 + num2;
+        }
+
+    }
+}
---
+++ OOProject/OOProject/FORM/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OOProject.CLASS;
+
+namespace OOProject.FORM
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        Class_1 c = new Class_1();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = 1;
+            int num2 = 10;
+            MessageBox.Show(c.number(num1, num2).ToString());
+        }
+    }
+}
