0041b10af260220cdfc011007bff3ca432a3b312
raineirserinas <raineirserinas@gmail.com>
2016-02-24 11:16:27 +0800

    Code igniter image upload sample for snippet

---
+++ ci_fileupload/application/controllers/Fileupload.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Fileupload extends CI_Controller
+{
+
+
+    public function index()
+    {
+        $this->load->view('upload');
+    }
+
+    public function do_upload()
+    {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size'] = '10000';
+        $config['max_width'] = '1024';
+        $config['max_height'] = '768';
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload()) {
+            $error = array('error' => $this->upload->display_errors());
+            print_r($error);
+            //$this->load->view('upload', $error);
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+            echo "Success!";
+            //$this->load->view('upload_success', $data);
+        }
+    }
+}
---
+++ ci_fileupload/application/views/upload.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Upload</title>
+</head>
+<body>
+
+
+<form action="http://127.0.0.1/ci_fileupload/fileupload/do_upload" enctype="multipart/form-data" method="post"
+      accept-charset="utf-8">
+
+
+    <input type="file" name="userfile" size="20"/>
+
+    <br/><br/>
+
+    <input type="submit" value="upload"/>
+
+</form>
+
+</body>
+</html>
