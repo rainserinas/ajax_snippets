790325c0d7f92c722ae48523ef8fce383a0de64d
rainserinas <raineirserinas@gmail.com>
2016-03-26 20:04:16 -0700

    Update wordpress guide

--- index.php
+++ index.php
@@ -18,3 +18,22 @@ if (have_posts()) {
 get_footer();
 ?>
 
+
+<?php 
+//Basic the loop on wordpress
+
+
+if(have_posts()) :
+	
+	while(have_posts()) : the_post();
+		//output content however we please here
+	endwhile;
+	
+	else;
+		//fallback no content message here
+
+endif;
+
+
+?>
+
